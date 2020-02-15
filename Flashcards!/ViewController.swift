e469080d6ad666f91a21a5708937e4447fcccb91
Halli Lacanlale <hlacan2@uic.edu>
2020-02-14 23:27:59 -0600

    Initial Commit

---
+++ Flashcards!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards!
+//
+//  Created by Halli  Lacanlale on 2/14/20.
+//  Copyright © 2020 Halli Lacanlale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
